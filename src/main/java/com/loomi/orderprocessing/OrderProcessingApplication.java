c0d22aa19c5c0aff923eeb47e1d8c65948f0a2bb
mateus.prado <mateus.prado@uniqhub.com.br>
2026-05-15 02:12:31 -0300

    fix: fix zookeeper container initialization preventing kafka startup

--- src/main/java/com/loomi/orderprocessing/OrderProcessingApplication.java
+++ src/main/java/com/loomi/orderprocessing/OrderProcessingApplication.java
@@ -2,7 +2,9 @@ package com.loomi.orderprocessing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class OrderProcessingApplication {
     public static void main(String[] args) {
